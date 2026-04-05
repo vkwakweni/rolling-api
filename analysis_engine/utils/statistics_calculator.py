48dd786ce32d55317a7cd271258627c67ca7359e
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2026-04-05 21:46:50 +0200

    implemented tests for statistics calculator

--- analysis_engine/utils/statistics_calculator.py
+++ analysis_engine/utils/statistics_calculator.py
@@ -20,28 +20,38 @@ class StatisticsCalculator:
     def standard_deviation(self, values):
         self.validate_numeric_sample(values)
         if len(values) <= 1:
-            raise InsufficientSampleSizeError(len(values)) # TODO should this just return None
+            return None
         return statistics.stdev(values)
     
     def pooled_standard_deviation(self, group_a, group_b):
         """
-        Weighted pooled standard deviation
-        Source: https://resources.nu.edu/statsresources/cohensd
+        Pool standard deviation
+        Source: https://www.statisticshowto.com/pooled-standard-deviation/
         """
         self.validate_numeric_sample(group_a)
         self.validate_numeric_sample(group_b)
-        variance_group_a = statistics.variance(group_a)
-        variance_group_b = statistics.variance(group_b)
         pooled_stdev = math.sqrt(
-            ((len(group_a) - 1) * variance_group_a + (len(group_b) - 1) * variance_group_b) / (len(group_a) + len(group_b) - 2)
+            ( self.standard_deviation(group_a)**2 + self.standard_deviation(group_b)**2 ) / 2
         )
         return pooled_stdev
     
-    def pooled_standard_error(self, group_a, group_b):
+    def cohen_pooled_standard_deviation(self, group_a, group_b):
+        """
+        Weighted pooled standard deviation
+        Source: https://resources.nu.edu/statsresources/cohensd
+        """
+        stdev_a = self.standard_deviation(group_a)
+        stdev_b = self.standard_deviation(group_b)
+        pooled_stdev = math.sqrt(
+            ((len(group_a) - 1) * stdev_a**2 + (len(group_b) - 1) * stdev_b**2) / (len(group_a) + len(group_b) - 2)
+        )
+        return pooled_stdev
+    
+    def cohen_pooled_standard_error(self, group_a, group_b):
         """
         Source: https://stats.libretexts.org/Bookshelves/Introductory_Statistics/Statistics%3A_Open_for_Everyone_(Peter)/08%3A_Independent_Samples_t-Tests/8.03%3A_The_Independent_Samples_t-Test_Formula
         """
-        pooled_std_error = self.pooled_standard_deviation(group_a, group_b) * \
+        pooled_std_error = self.cohen_pooled_standard_deviation(group_a, group_b) * \
                                 (math.sqrt(
                                     (len(group_a) + len(group_b)) / (len(group_a) * len(group_b))
                                 ))
@@ -54,27 +64,18 @@ class StatisticsCalculator:
         TODO throw warning for length
         TODO look at the actual difference between this and Hedge's g
         """
-        mean_diff = self.mean(group_a) - self.mean(group_b)
-        pooled_stdev = math.sqrt(
-            (self.standard_deviation(group_a) + self.standard_deviation(group_b)) / 2
-        ) # TODO can this be 0?
-        return mean_diff / pooled_stdev
-
-    def hedges_g(self, group_a, group_b):
-        """
-        This is best for smaller sample sizes
-        """
-        mean_diff = self.mean(group_a) - self.mean(group_b)
-        pooled_stdev = self.pooled_standard_deviation(group_a, group_b)
+        mean_diff = abs(self.mean(group_a) - self.mean(group_b))
+        pooled_stdev = self.cohen_pooled_standard_deviation(group_a, group_b)
         return mean_diff / pooled_stdev
 
     def independent_t_test(self, group_a, group_b):
         """
         TODO I don't think this is what we used in the paper
         """
-        mean_diff = self.mean(group_a) - self.mean(group_b)
-        pooled_sterror = self.pooled_standard_error(group_a, group_b)
-        return mean_diff / pooled_sterror
+        mean_diff = abs(self.mean(group_a) - self.mean(group_b))
+        pooled_stdev = self.cohen_pooled_standard_deviation(group_a, group_b)
+        pooled_sterror = self.cohen_pooled_standard_error(group_a, group_b)
+        return mean_diff / (pooled_stdev * pooled_sterror)
 
     @staticmethod
     def validate_numeric_sample(values):
@@ -87,12 +88,13 @@ class StatisticsCalculator:
                 raise NonNumericError(value)
     
     @staticmethod
-    def safe_divide(numerator, denominator: float):
+    def safe_divide(denominator: float) -> float:
         """
         TODO this is should replace division in this logic, and handle 0's
         """
         if denominator == 0:
             raise ZeroDivisionError
+        return denominator
     
 
 class CalculatorError(Exception):
---
+++ tests/unit/analysis_engine/utils/test_statistics_calculator.py
@@ -0,0 +1,122 @@
+import unittest
+
+from analysis_engine.utils.statistics_calculator import (StatisticsCalculator,
+                                                         EmptySampleError,
+                                                         NonNumericError,
+                                                         InsufficientSampleSizeError)
+
+class TestStatisticsCalculator(unittest.TestCase):
+    calculator = StatisticsCalculator()
+    invalid_input = [1, 2, "a", 4, 5]
+    empty_input = []
+
+    correct_input_a = [1, 2, 3, 4, 5]
+    correct_input_b = [2, 4, 6, 8, 10]
+    
+    # calculated manually
+    mean_a = 3.0
+    median_a = 3.0
+    stdev_a = 1.58
+    stdev_b = 3.16
+    pooled_stdev_ab = 2.50
+    cohen_pooled_stdev_ab = 2.50
+    cohen_pooled_stderror_ab = 1.58
+    cohens_d_ab = 1.20
+    independent_t_ab = 0.76
+
+    # MAIN METHODS
+    def test_mean(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.mean(self.invalid_input)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.mean(self.empty_input)
+
+        self.assertEqual(self.calculator.mean(self.correct_input_a),
+                         self.mean_a)
+
+    def test_median(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.median(self.invalid_input)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.median(self.empty_input)
+
+        self.assertEqual(self.calculator.median(self.correct_input_a),
+                         self.median_a)
+
+    def test_standard_deviation(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.standard_deviation(self.invalid_input)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.standard_deviation(self.empty_input)
+
+        self.assertEqual(round(self.calculator.standard_deviation(self.correct_input_a), 2),
+                         self.stdev_a)
+        
+    def test_pooled_standard_deviation(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.pooled_standard_deviation(self.invalid_input, self.correct_input_a)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.pooled_standard_deviation(self.empty_input, self.correct_input_a)
+
+        self.assertEqual(self.calculator.pooled_standard_deviation(self.correct_input_a, self.correct_input_b),
+                         self.pooled_stdev_ab)
+        
+    def test_cohen_pooled_standard_deviation(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.cohen_pooled_standard_deviation(self.invalid_input, self.correct_input_a)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.cohen_pooled_standard_deviation(self.empty_input, self.correct_input_a)
+
+        self.assertEqual(round(self.calculator.cohen_pooled_standard_deviation(self.correct_input_a, self.correct_input_b), 2),
+                         self.cohen_pooled_stdev_ab)
+
+    def  test_pooled_standard_error(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.cohen_pooled_standard_error(self.invalid_input, self.correct_input_a)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.cohen_pooled_standard_error(self.empty_input, self.correct_input_a)
+
+        self.assertEqual(round(self.calculator.cohen_pooled_standard_error(self.correct_input_a, self.correct_input_b), 2),
+                         self.cohen_pooled_stderror_ab)
+
+    def test_cohens_d(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.cohens_d(self.invalid_input, self.correct_input_a)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.cohens_d(self.empty_input, self.correct_input_a)
+
+        self.assertEqual(round(self.calculator.cohens_d(self.correct_input_a, self.correct_input_b), 2),
+                         self.cohens_d_ab)
+
+    def test_independent_t_test(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.independent_t_test(self.invalid_input, self.correct_input_a)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.independent_t_test(self.empty_input, self.correct_input_a)
+
+        self.assertEqual(round(self.calculator.independent_t_test(self.correct_input_a, self.correct_input_b), 2),
+                         self.independent_t_ab)
+
+    # HELPER / STATIC METHODS
+    def test_validate_numeric_sample(self):
+        with self.assertRaises(NonNumericError):
+            self.calculator.validate_numeric_sample(self.invalid_input)
+
+        with self.assertRaises(EmptySampleError):
+            self.calculator.validate_numeric_sample(self.empty_input)
+
+        self.assertIsNone(self.calculator.validate_numeric_sample(self.correct_input_a))
+    
+    def test_safe_divide(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calculator.safe_divide(0)
+
+        self.assertEqual(self.calculator.safe_divide(10), 10)
